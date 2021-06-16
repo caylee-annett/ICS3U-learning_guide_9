e20d8326e9a0c5d792501c1b17d49eb2b862a615
caylee-annett <82886901+caylee-annett@users.noreply.github.com>
2021-06-16 10:25:49 -0400

    Update code.py

--- code.py
+++ code.py
@@ -11,19 +11,119 @@ import time
 
 import constants
 
+def splash_scene():
+    # This function is the splash scene
 
-#!/usr/bin/env python3
+    # get sound ready
+    coin_sound = open("coin.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
 
-# Created by: Caylee Annett
-# Created on: June 2021
-# This is the "Flappy Fish" program on the PyBadge
+    # image banks for CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
 
-import ugame
-import stage
-import random
-import time
+    # create a stage for the background to show up on and set the frame
+    #   rate to 60fps
+    # set the background to image 0 in the image bank
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
+                            constants.SCREEN_Y)
 
-import constants
+    # used this program to split the image into tile:
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+
+    # create a stage for the background to show up on and set the frame
+    #   rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers of all sprites
+    game.layers = [background]
+    # render all sprites
+    game.render_block()
+
+    # Repeat forever, game loop
+    while True:
+        # wait for 2 seconds
+        time.sleep(2.0)
+        menu_scene()
+
+
+def menu_scene():
+    # This function is the menu scene
+
+    # image banks for CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # add text objects
+    text = []
+    text1 = stage.Text(width=29, height=12, font=None,
+                       palette=constants.BLUE_PALETTE, buffer=None)
+    text1.move(20, 10)
+    text1.text("MT Game Studios")
+    text.append(text1)
+
+    text2 = stage.Text(width=60, height=12, font=None,
+                       palette=constants.BLUE_PALETTE, buffer=None)
+    text2.move(40, 60)
+    text2.text("FLAPPY FISH")
+    text.append(text2)
+
+    text3 = stage.Text(width=29, height=12, font=None,
+                       palette=constants.BLUE_PALETTE, buffer=None)
+    text3.move(40, 110)
+    text3.text("Press START")
+    text.append(text3)
+
+    # set the background to image 0 in the image bank and the size
+    #   to (10x8 tiles of 16x16)
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_GRID_X,
+                            constants.SCREEN_GRID_Y)
+
+    # create a stage for the background to show up on and set the frame
+    #   rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers of all sprites
+    game.layers = text + [background]
+    # render all sprites
+    game.render_block()
+
+    # Repeat forever, game loop
+    while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_START != 0:
+            game_scene()
+
+        # redraw Sprites
+        game.tick()  # wait until refresh rate finishes
 
 
 def game_scene():
@@ -177,9 +277,4 @@ def game_scene():
 
 
 if __name__ == "__main__":
-    game_scene()
-
-
-
-if __name__ == "__main__":
-    game_scene()
+    splash_scene()
